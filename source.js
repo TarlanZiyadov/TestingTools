9e092999b586cc986cb546fb5332b3d09321c3cc
TarlanZiyadov <tarlan.ziyadov@gmail.com>
2020-06-09 00:02:07 +0200

    Main page icon added

--- source.js
+++ source.js
@@ -93,17 +93,11 @@
   $(function(){
       $('.sidenav').hover(function(){
           $(this).animate({width:'28vmin'},50);
-          $('.footer').animate({width:'28vmin'},50);
-       
         $('.tablinks span').show();
-        $('[id^=copyrighttxt]').show();
   
       },function(){
           $(this).animate({width:'9vmin'},50);
-          $('.footer').animate({width:'0px'},50);
-  
           $('.tablinks span').hide();
-          $('[id^=copyrighttxt]').hide();
   
       }).trigger('mouseleave');
   });
