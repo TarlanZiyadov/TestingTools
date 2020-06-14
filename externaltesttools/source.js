8b32aa7499d952611c0bcb146102e9931241c67c
TarlanZiyadov <tarlan.ziyadov@gmail.com>
2020-06-15 00:22:22 +0200

    new page for external test tools developed

---
+++ externaltesttools/source.js
@@ -0,0 +1,177 @@
+
+  // this function is for Jmeter card
+  function openJmeter(evt, jmeter) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(jmeter).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+
+// this function is for Memreduct card
+function openMemreduct(evt, memreduct) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(memreduct).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+// this function is for Memtest card
+function openMemtest(evt, memtest) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(memtest).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+    
+ 
+// this function is for RapidReporter card
+function openRapidReporter(evt, rapidReporter) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(rapidReporter).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+ 
+// this function is for Wireshark card
+function openWireshark(evt, wireshark) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(wireshark).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+ 
+// this function is for Fiddler card
+function openFiddler(evt, fiddler) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(fiddler).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// this function is for Cypress io card
+function openCypress(evt, cypress) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cypress).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+// this function is for Selenium WebDriver card
+function openSelenium(evt, selenium) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(selenium).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// this function is for Xmind card
+function openXmind(evt, xmind) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(xmind).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+
+ // this jquery is for animation of sidebar
+  $(function(){
+      $('.sidenav').hover(function(){
+          $(this).animate({width:'28vmin'},50);
+        $('.tablinks span').show();
+  
+      },function(){
+          $(this).animate({width:'9vmin'},50);
+          $('.tablinks span').hide();
+  
+      }).trigger('mouseleave');
+  });
+
+  // This function is for footer css
+  $(function () { 
+  $('#footerMainPage').css("position", "fixed");
+  $('#footerMainPage').css("bottom", "0");   
+});
+
+// This function is for footer image css
+$(function () { 
+  $('#footerImg').css("position", "fixed");
+  $('#footerImg').css("bottom", "-2vmin");   
+});
