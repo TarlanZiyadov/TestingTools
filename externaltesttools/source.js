3cf91af7584766512f3c5f9d1b66dc6f42154d58
TarlanZiyadov <tarlan.ziyadov@gmail.com>
2020-06-22 21:47:52 +0200

    database added and configurated

--- externaltesttools/source.js
+++ externaltesttools/source.js
@@ -17,21 +17,7 @@
     evt.currentTarget.className += " active";
   }
   
-  // Here we show the first card on load always 
-  window.onload =function(){
-    let i, tabcontent;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      if(i==0){
-        tabcontent[i].style.display = "block";
-        if(i!=9){
-          $('body').css("width", "100vw");
-        }
-      }
-    }
-  }
-
-
+ 
 // this function is for Memreduct card
 function openMemreduct(evt, memreduct) {
   let i, tabcontent, tablinks;
@@ -239,3 +225,58 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-169533728-1');
+
+window.onload=function(){
+// Here we show the first card on load always 
+  let i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      if(i==0){
+        tabcontent[i].style.display = "block";
+        if(i!=9){
+          $('body').css("width", "100vw");
+        }
+      }
+    }
+
+    // Here we download content from database
+  let firebaseConfig = {
+    apiKey: "AIzaSyBc8ysyL8bIwcLWEj8y-5ZMXUK6NRamxCA",
+    authDomain: "db-web-498d2.firebaseapp.com",
+    databaseURL: "https://db-web-498d2.firebaseio.com",
+    projectId: "db-web-498d2",
+    storageBucket: "db-web-498d2.appspot.com",
+    messagingSenderId: "623170844206",
+    appId: "1:623170844206:web:64e24c587c46542f6fd1bf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+    let databaseJmeter = firebase.database().ref().child('externaltesttools/jmeter');
+    let databaseExternalTestToolsMain = firebase.database().ref().child('externaltesttools/mainpageText');
+   
+    databaseJmeter.child('descriptionFirst').on('value', function(snapshot) {
+    let descriptionFirst = snapshot.val();
+    let jmeterTextFirst = document.getElementById("descriptionJmeter");
+    jmeterTextFirst.textContent += descriptionFirst;
+    });
+
+    databaseJmeter.child('descriptionSecond').on('value', function(snapshot) {
+    let descriptionSecond = snapshot.val();
+    let jmeterTextSecond = document.getElementById("jemterFeature");
+    jmeterTextSecond.textContent += descriptionSecond;
+    });
+
+    databaseJmeter.child('listHeader').on('value', function(snapshot) {
+    let listHeader = snapshot.val();
+    let jmeterTextThird = document.getElementById("jmeterAbility");
+    jmeterTextThird.textContent += listHeader;
+    });
+
+    databaseExternalTestToolsMain.child('descriptionFirst').on('value', function(snapshot) {
+    let descriptionFirst = snapshot.val();
+    let mainTextFirst = document.getElementById("frontPagetxt");
+    mainTextFirst.textContent += descriptionFirst;
+    });
+
+}
