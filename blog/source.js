1a06fb825c1e02d2b98b94c30908e58126e38016
TarlanZiyadov <tarlan.ziyadov@gmail.com>
2020-07-07 17:11:19 +0200

    js change

--- blog/source.js
+++ blog/source.js
@@ -1,4 +1,4 @@
-function openTab(evt, tabName) {
+function openTab(evt, tabContentId) {
     // Declare all variables
     var i, tabcontent, tablinks;
   
@@ -15,7 +15,7 @@ function openTab(evt, tabName) {
     }
   
     // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabContentId).style.display = "block";
     evt.currentTarget.className += " active";
   }
 
