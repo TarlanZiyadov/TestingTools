a099407ed500e27bf3c67c652bcb22d9e14c4c3c
TarlanZiyadov <tarlan.ziyadov@gmail.com>
2020-12-30 04:09:52 +0100

    refactor source code to follow  DRY and KISS principle

--- source.js
+++ source.js
@@ -1,81 +1,77 @@
- 
- // this function animates downloading banner in down side of page
- function downloading() {
-    let dwnload = document.getElementById("waitDownloading");
-    dwnload.className = "show";
-    setTimeout(function(){ dwnload.className = dwnload.className.replace("show", ""); }, 2000);
-  }
-  
-  // this function shows tabcontent
-  function openCard(evt, tabContentId) {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabContentId).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+// this function animates downloading banner in down side of page
+function downloading() {
+  let dwnload = document.getElementById("waitDownloading");
+  dwnload.className = "show";
+  setTimeout(function () { dwnload.className = dwnload.className.replace("show", ""); }, 2000);
+}
 
- // this jquery is for animation of sidebar
-  $(function(){
-      $('.sidenav').hover(function(){
-          $(this).animate({width:'35vmin'},50);
-        $('.tablinks span').show();
-  
-      },function(){
-          $(this).animate({width:'9vmin'},50);
-          $('.tablinks span').hide();
-  
-      }).trigger('mouseleave');
-  });
+// this function shows tabcontent
+function openCard(evt, tabContentId) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabContentId).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-  // This function is for footer css
-  $(function () { 
+// this jquery is for animation of sidebar
+$(function () {
+  $('.sidenav').hover(function () {
+    $(this).animate({ width: '35vmin' }, 50);
+    $('.tablinks span').show();
+
+  }, function () {
+    $(this).animate({ width: '9vmin' }, 50);
+    $('.tablinks span').hide();
+
+  }).trigger('mouseleave');
+});
+
+// This function is for footer css
+$(function () {
   $('#footerMainPage').css("position", "fixed");
-  $('#footerMainPage').css("bottom", "0");   
+  $('#footerMainPage').css("bottom", "0");
 });
 
 // This function is for footer image css
-$(function () { 
+$(function () {
   $('#footerImg').css("position", "fixed");
-  $('#footerImg').css("bottom", "-2vmin");   
+  $('#footerImg').css("bottom", "-2vmin");
 });
-
 
 // For google analytics
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'UA-169533728-1');
 
-
 // Here we show the first card on load always 
-document.addEventListener('DOMContentLoaded', (e)=> {
+document.addEventListener('DOMContentLoaded', (e) => {
   let i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
-    if(i==0){
+    if (i == 4) {
       tabcontent[i].style.display = "block";
     }
   }
-
   // animation for haptic effect
-  setTimeout(function() {
-      $('.haptic-on').addClass('haptic-swing');
-      $('.haptic-swing').removeClass('haptic-on');
-   }, 4000);
+  setTimeout(function () {
+    $('.haptic-on').addClass('haptic-swing');
+    $('.haptic-swing').removeClass('haptic-on');
+  }, 4000);
 }, true);
 
 // close sidebar on body click for touch screens
-document.getElementById('main').addEventListener('click', ()=> {
+document.getElementById('main').addEventListener('click', () => {
   closeSidebar();
 });
-function closeSidebar (){
-  $('.sidenav').animate({width:'9vmin'},50);
+function closeSidebar() {
+  $('.sidenav').animate({ width: '9vmin' }, 50);
   $('.tablinks span').hide();
 }
